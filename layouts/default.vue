03a133189d1421f371e2ddc4b7f46b2e6f394507
Surry <50641921+surryjm@users.noreply.github.com>
2021-04-01 15:09:03 -0700

    finished lesson9

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div id="app">
+      <a href="#main-content" class="hidden">Skip to main content</a>
+      <div class="container-fluid">
+        <Header /> <!--HEADER COMPONENT-->
+        <Nuxt />
+        <Footer />  <!--FOOTER COMPONENT-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
