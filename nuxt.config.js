359cc7f2d9f211be6d4a5c3b8b9bb6af11edb48d
Surry <50641921+surryjm@users.noreply.github.com>
2021-04-02 10:45:01 -0700

    removed nuxt.config.js update

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,11 +60,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  },
-  
-  generate: {
-    minify: {
-      collapseWhitespace: false
-    }
   }
+
 }
